41204fc36b6acec05f0a40cee388f68c05d1e1a7
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-09-06 14:04:42 -0500

    se cambian direcciones de api y tech en produccion

--- src/utils/SecretsConstant.js
+++ src/utils/SecretsConstant.js
@@ -7,8 +7,8 @@
 
 var SecretsConstant = {
 
-  HOST_API: 'http://localhost:3000',
-  TECHNOLOGY_API: 'http://localhost:3001',
+  HOST_API: 'http://181.143.72.70:11000',
+  TECHNOLOGY_API: 'http://181.143.72.70:9000',
   PASSWORD_AUT_TOKEN: '/s4P13Nc1A2014mEdCoL/'
 };
 
