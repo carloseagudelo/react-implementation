a7752e49b240c9c0d8759e262680e3a3721e56c4
Carlos Enrique Agudelo <carlos.agudelo@sapiencia.gov.co>
2017-09-22 19:47:09 -0500

    Compila para pruebas

--- src/utils/SecretsConstant.js
+++ src/utils/SecretsConstant.js
@@ -1,13 +1,12 @@
 /* Descripcion: Clase que contiene las constantes de seguridad a usar en el desarrollo del aplicativo
-   Autor: Carlos Agudelo
-   Contacto: agudelo.carlos@hotmail.es
+  Autor: Carlos Agudeo   Contacto: agudelo.carlos@hotmail.es
    Fecha de creación: 10 de Junio
    Fecha de modificacion: 10 de Junio */
 
 
 var SecretsConstant = {
-  HOST_API: 'http://181.143.72.70:11000',
-  TECHNOLOGY_API: 'http://181.143.72.70:9000',
+  HOST_API: 'http://192.168.1.12:4500',
+  TECHNOLOGY_API: 'http://192.168.1.12:3500',
   PASSWORD_AUT_TOKEN: '/s4P13Nc1A2014mEdCoL/'
 };
 
