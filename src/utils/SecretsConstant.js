ba96750fabac5b9380e4f86c628df7659698104d
Carlos Agudelo <carloskikea@gmail.com>
2017-10-18 20:26:06 -0500

    Se copilan archivos para pruebas

--- src/utils/SecretsConstant.js
+++ src/utils/SecretsConstant.js
@@ -5,10 +5,10 @@
 
 
 var SecretsConstant = {
-  HOST_API: 'http://181.143.72.70:11000',
-  TECHNOLOGY_API: 'http://181.143.72.70:9000',
+  HOST_API: 'http://192.168.1.12:4500',
+  TECHNOLOGY_API: 'http://192.168.1.12:3500',
   PASSWORD_AUT_TOKEN: '/s4P13Nc1A2014mEdCoL/',
-  PP_API: ''
+  PP_API: 'http://192.168.1.12:3000'
 };
 
 module.exports = SecretsConstant;
